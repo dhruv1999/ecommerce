7f2e1798184e37d4bb376e6d1f8320a508d26dc5
dhruv1999 <dhruv.anita100@gmail.com>
2019-08-11 18:48:00 +0530

    productseeder updated

--- seed/productseeder.js
+++ seed/productseeder.js
@@ -5,37 +5,37 @@ mongoose.connect('mongodb://localhost/shopping', { useNewUrlParser: true })
 var products = [
 	new Product({
 		imagePath:
-			'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+			'https://dks.scene7.com/is/image/GolfGalaxy/18NIKWRMX270XXXXXLFS_Black_Cream?wid=1080&fmt=jpg',
 		title: 'Nike ',
 		description: 'Nike Airmax',
 		price: 10
 	}),
 	new Product({
 		imagePath:
-			'http://eu.blizzard.com/static/_images/games/wow/wallpapers/wall2/wall2-1440x900.jpg',
-		title: 'World of Warcraft Video Game',
-		description: 'Also awesome? But of course it was better in vanilla ...',
+			'https://dks.scene7.com/is/image/GolfGalaxy/19ADIMPLSBSTSLRRDMNS_Black_Solar_Red?wid=1080&fmt=jpg',
+		title: 'Adidas',
+		description: 'Best for running',
 		price: 20
 	}),
 	new Product({
 		imagePath:
-			'https://support.activision.com/servlet/servlet.FileDownload?file=00PU000000Rq6tz',
-		title: 'Call of Duty Video Game',
-		description: "Meh ... nah, it's okay I guess",
+			"https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+		title: 'Nike',
+		description: "Nike for Indoors",
 		price: 40
 	}),
 	new Product({
 		imagePath:
-			'https://pmcdeadline2.files.wordpress.com/2014/02/minecraft__140227211000.jpg',
-		title: 'Minecraft Video Game',
-		description: 'Now that is super awesome!',
+			'https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/1/3/136354_01.jpg',
+		title: 'Yeezy',
+		description: 'Best for Fashion',
 		price: 15
 	}),
 	new Product({
 		imagePath:
-			'https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg',
-		title: 'Dark Souls 3 Video Game',
-		description: 'I died!',
+			'https://dsw.scene7.com/is/image/DSWShoes/404995_001_ss_01?$pdp-image$',
+		title: 'Vans',
+		description: 'Best canvas shoes',
 		price: 50
 	})
 ]
