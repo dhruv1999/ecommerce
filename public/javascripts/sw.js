ce4a07ac25b7960868b1abd2699392096252a648
aashutoshjaswani <ashutosh.jaswani444@gmail.com>
2019-11-04 00:07:35 +0530

    sign up removed

--- public/javascripts/sw.js
+++ public/javascripts/sw.js
@@ -1,60 +1,39 @@
-var cacheName = 'tiendaw-v2'
-var filesToCache = [
-	'../public/images',
-	'/views',
-	'/stylesheets/style.css',
-	'/images/bgimg.jpeg',
-	'sw.js',
-	'/app.js'
-]
 
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			console.info('[sw.js] cached all files')
-			return cache.addAll(filesToCache)
-		})
-	)
-})
 
+var cacheName = 'v1:static';
+
+// during the install phase you usually want to cache static assets
+self.addEventListener('install', function(e) {
+    // once the SW is installed, go ahead and fetch the resources to make this work offline
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+				'/',
+				'../public/images',
+				'/views',
+				'/stylesheets/style.css',
+				'/images/bgimg.jpeg',
+				'sw.js',
+				'/app.js'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
+});
+
+// when the browser fetches a url
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			if (response) {
-				return response
-			} else {
-				// clone request stream
-				// as stream once consumed, can not be used again
-				var reqCopy = event.request.clone()
-
-				return fetch(reqCopy, { credentials: 'include' }) // reqCopy stream consumed
-					.then(function(response) {
-						// bad response
-						// response.type !== 'basic' means third party origin request
-						if (
-							!response ||
-							response.status !== 200 ||
-							response.type !== 'basic'
-						) {
-							return response // response stream consumed
-						}
-
-						// clone response stream
-						// as stream once consumed, can not be used again
-						var resCopy = response.clone()
-
-						// ================== IN BACKGROUND ===================== //
-
-						// add response to cache and return response
-						caches.open(cacheName).then(function(cache) {
-							return cache.put(reqCopy, resCopy) // reqCopy, resCopy streams consumed
-						})
-
-						// ====================================================== //
-
-						return response // response stream consumed
-					})
-			}
-		})
-	)
-})
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
